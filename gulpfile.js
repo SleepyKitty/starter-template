4390fb24caf9b9a1a1e9c3e267f4f969304cc254
Mike <targethunter0@gmail.com>
2018-09-27 13:56:16 +0300

    add starter template

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('serve', ['sass'], function () {
+
+  browserSync.init({
+    server: {
+      baseDir: "src"
+    },
+    notify: false
+  });
+
+  gulp.watch("src/sass/*.scss", ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function () {
+  return gulp.src("src/sass/*.scss")
+      .pipe(sass().on('error', function (error) {
+        console.log(error);
+      }))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(gulp.dest("src/css"))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
